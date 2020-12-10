7e644e156a5fdd3ad1d630d2bbe650d1df40a628
ShivamRohilllaa <59178005+ShivamRohilllaa@users.noreply.github.com>
2020-12-10 14:50:02 +0530

    signup works fine

--- shirts/forms.py
+++ shirts/forms.py
@@ -5,43 +5,30 @@ from django.core.exceptions import ValidationError
 from .models import *
 
 
-class CustomerCreationForm(UserCreationForm):
-    username = forms.EmailField(max_length=50, label="Email",
-        # widget= forms.EmailInput(attrs={'class':'form-control'})
-        ) #Isme emailfield isliye liye hai qki humne isme username use ni kiya isme, username ki jagah humne isme email use ki hai to that's why we use this!!
-    
-    first_name = forms.CharField(max_length=50,
-        # widget= forms.TextInput(attrs={'class':'form-control'})
-        )
-    
-    last_name = forms.CharField(max_length=50,
-        # widget= forms.TextInput(attrs={'class':'form-control'})
-    )
-    
-    password1 = forms.PasswordInput(
-        # widget= forms.PasswordInput(attrs={'class':'form-control'})
-                           )
-    
-    password2 = forms.PasswordInput(
-        # widget= forms.PasswordInput(attrs={'class':'form-control'})
-        )
+class CustomerAuthForm(AuthenticationForm):
+    username = forms.EmailField(required=True , label="Email")
 
-    #Apply Validations on a particular field
+class CustomerCreationForm(UserCreationForm):
+    
+    username = forms.EmailField(required=True , label="Email" )
+    first_name = forms.CharField(required=True , label="First Name")
+    last_name = forms.CharField(required=True , label="Last Name")
+    class Meta:
+        model = User
+        fields = ['username' ,'first_name' , "last_name" ]
 
     def clean_first_name(self):
         value = self.cleaned_data.get('first_name')
-        if len(value.strip()) < 4:
-            raise ValidationError("Please Enter 4 Character Name") 
-
+        if len(value.strip()) < 4 :
+            raise ValidationError("First Name must be 4 char long...")
+        return value.strip()
+    
     def clean_last_name(self):
         value = self.cleaned_data.get('last_name')
-        if len(value.strip()) < 4:
-            raise ValidationError("Please Enter 4 Character Name") 
-        
+        if len(value.strip()) < 4 :
+            raise ValidationError("Last Name must be 4 char long...")
+        return value.strip()
 
-    class Meta(UserCreationForm):
-        model = User
-        fields = ('username', 'first_name','last_name', 'password1', 'password2')
 
 
 class Customerloginform(AuthenticationForm):
